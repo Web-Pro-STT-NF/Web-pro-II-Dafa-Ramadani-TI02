b2d3530fa70224a8a7b5899b21c8e812549ba5ee
dappp <92776087+Dafa21065@users.noreply.github.com>
2022-04-24 15:09:06 +0700

    Add files via upload

---
+++ Priject1_UTS/Praktikum-5/dispenser/class_dispenser.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dafa Ramadani Dispenser</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+<div class="text-center">
+    <div class="card mt-3 ms-4" style="width: 50rem;">
+    <h1 class="card-header">Data Dispenser </h1>
+    <div class="card-body">
+<?php
+    include_once "class_dispenser.php";
+
+
+class Dispenser{
+  protected $volume;
+  protected $hargaSegelas;
+  private $volumeGelas;
+  public $namaMinuman;
+
+  function  volume($vol){
+    $this->volume = $vol;
+  }
+}
+
+class Minuman extends Dispenser
+{
+  public $uang;
+  function __construct($uang, $namaMinuman, $volumeGelas, $harga)
+  {
+    $this->uang = $uang;
+    $this->namaMinuman = $namaMinuman;
+    $this->hargaSegelas = $harga;
+    $this->volumeGelas = $volumeGelas;
+  }
+
+  function transaksi($uang)
+  {
+    $this->uang = $uang;
+    return  $this->uang - $this->hargaSegelas;
+  }
+
+  function volumeSetelahdibeli()
+  {
+    $this->volume = $this->volume - $this->volumeGelas;
+    return $this->volume;
+  }
+
+  function cetak()
+  {
+
+
+    echo '<h3>'. 'Selamat Datang di Toko DARAM '.'</h3>';
+    echo '<hr>';
+    echo 'Nama Minuman : ' . $this->namaMinuman . "<br>";
+    echo 'Uang Saat ini : ' . $this->uang . "<br>";
+    echo 'kapasistas (volume) Dispenser : ' . $this->volume . 'ml' . "<br>";
+    echo 'Harga Bayar Segelas Minuman : ' . $this->hargaSegelas . "<br>";
+    echo 'Volume Dispenser setelah dibeli : ' . $this->volumeSetelahDibeli() . 'ml' . "<br>";
+    echo 'Sisa uang: ' . $this->transaksi($this->uang);
+    echo '<hr>';
+  }
+}
+
+$minuman = new Minuman(10000, 'Lemineral', 1000, 4000);
+$minuman->transaksi($minuman->uang);
+$minuman->volume(4000);
+$minuman->cetak();
